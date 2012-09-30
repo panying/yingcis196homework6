c1134dd8d363705900261497c933709ec7acdc9f
Ying <candypan515@gmail.com>
2012-09-29 22:16:05 -0400

    second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-http_basic_authenticate_with :name => "dhh", :password => "secret", :only => :destroy
+
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
@@ -14,7 +14,6 @@ end
 
 class PostsController < ApplicationController
 
-http_basic_authenticate_with :name => "dhh", :password => "secret", :only => :destroy
 
 
 def new
